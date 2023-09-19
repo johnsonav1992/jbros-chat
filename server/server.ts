6bae11562728082aacc39a80884c0b12faeb2ce1
Alex Johnson <johnsonav1992@gmail.com>
2023-09-18 22:13:16 -0500

    got everything set up for now...

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,9 @@
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 
 const io = new Server();
 
-io.on( "connection", ( socket ) => {
-    console.log( `It's alive!!`, socket.connected )
+io.on( "connection", ( socket: Socket ) => {
+    console.log( `It's alive!!!`, socket.connected )
 } );
 
 io.listen( 4000 );
