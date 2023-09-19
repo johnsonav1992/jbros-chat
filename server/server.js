c1e0931093f22c587f934f61366029c7c6d0aae2
Alex Johnson <johnsonav1992@gmail.com>
2023-09-19 05:53:35 -0500

    more updates for now

--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
 "use strict";
-import { Server } from "socket.io";
-var io = new Server({
-    cors: { origin: '*' }
-});
+Object.defineProperty(exports, "__esModule", { value: true });
+var socket_io_1 = require("socket.io");
+var io = new socket_io_1.Server();
 io.on("connection", function (socket) {
-    console.log("It's alive!!!", socket.connected);
+    console.log("It's alive!!!", socket.id);
 });
 io.listen(4000);
--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,9 @@
 import { Server, Socket } from "socket.io";
 
-const io = new Server( { 
-    cors: { origin: '*' }
-} );
+const io = new Server();
 
 io.on( "connection", ( socket: Socket ) => {
-    console.log( `It's alive!!!`, socket.connected )
+    console.log( `It's alive!!!`, socket.id )
 } );
 
 io.listen( 4000 );
