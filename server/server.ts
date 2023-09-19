bf2aa2537eb3122a5f8210cd31927149126ab230
Alex Johnson <johnsonav1992@gmail.com>
2023-09-18 21:56:24 -0500

    caps

--- server/server.ts
+++ server/server.ts
@@ -3,7 +3,7 @@ import { Server } from "socket.io";
 const io = new Server();
 
 io.on( "connection", ( socket ) => {
-    console.log( `it's alive!!`, socket.connected )
+    console.log( `It's alive!!`, socket.connected )
 } );
 
 io.listen( 4000 );
