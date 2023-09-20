fdad3f5d764dce81842f60b9de851e3a4b9f984a
Alex Johnson <johnsonav1992@gmail.com>
2023-09-20 06:48:07 -0500

    added pinging for users entering the server

--- backend/index.ts
+++ backend/index.ts
@@ -2,11 +2,26 @@ import {
     Server
     , Socket
 } from "socket.io";
+import { 
+    ActiveUser
+    , ClientToServerEvents
+    , ServerToClientEvents
+} from "./types/types";
 
-const io = new Server( { cors: { origin: '*' } } );
+const io = new Server<
+    ClientToServerEvents
+    , ServerToClientEvents
+>( { cors: { origin: '*' } } );
 
-io.on( "connection", ( socket: Socket ) => {
-    console.log( `It's alive!!!`, socket.id )
+const activeUsers: Record<Socket['id'], ActiveUser> = {}
+
+io.on( "connection", socket => {
+    socket.on('new-user', userName => {
+        activeUsers[ socket.id ] = { socketId: socket.id, userName }
+        socket.broadcast.emit( 'user-connected', userName )
+
+        console.log( `A new user has logged on - ${ userName }, id: ${ socket.id }` )
+    })
 } );
 
 io.listen( 4000 );
---
+++ backend/types/types.ts
@@ -0,0 +1,12 @@
+export type ActiveUser = {
+    socketId: string;
+    userName: string;
+}
+
+export interface ServerToClientEvents {
+    'user-connected': (userName: ActiveUser['userName']) => void;
+}
+
+export interface ClientToServerEvents {
+    'new-user': (submittedUserName: ActiveUser['userName']) => void;
+}
