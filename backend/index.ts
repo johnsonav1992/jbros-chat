305265014bf2f2e26deff84fc1c4fdc796f8fce6
Alex Johnson <johnsonav1992@gmail.com>
2023-09-20 06:55:24 -0500

    multiline obj

--- backend/index.ts
+++ backend/index.ts
@@ -17,7 +17,10 @@ const activeUsers = new Map<Socket['id'], ActiveUser>();
 
 io.on( "connection", socket => {
     socket.on('new-user', submittedUserName => {
-        activeUsers.set(socket.id, { socketId: socket.id, userName: submittedUserName })
+        activeUsers.set(socket.id, { 
+            socketId: socket.id
+            , userName: submittedUserName 
+        } )
         socket.broadcast.emit( 'user-connected', submittedUserName )
 
         console.log( `A new user has logged on - ${ submittedUserName }, id: ${ socket.id }` )
