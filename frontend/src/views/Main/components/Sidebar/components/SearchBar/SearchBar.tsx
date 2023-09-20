23a779ac56fa385432595cf47b05b5894d571495
Eliot Johnson <eliotmjohnson@gmail.com>
2023-09-20 18:02:46 -0500

    Fixed Some Requests

--- frontend/src/views/Main/components/Sidebar/components/SearchBar/SearchBar.tsx
+++ frontend/src/views/Main/components/Sidebar/components/SearchBar/SearchBar.tsx
@@ -4,14 +4,6 @@ import styles from "./SearchBar.module.css";
 const SearchBar = () => {
 	const [isfocused, setIsFocused] = useState(false);
 
-	const focus = (): void => {
-		setIsFocused(true);
-	};
-
-	const unFocus = (): void => {
-		setIsFocused(false);
-	};
-
 	return (
 		<main className={styles.searchbarContainer}>
 			<span className={styles.searchbar}>
@@ -19,13 +11,17 @@ const SearchBar = () => {
 					className={styles.searchIcon}
 					src="/images/icons/icons8-search-150.png"
 				/>
-				<input onClick={focus} type="text" placeholder="Search" />
+				<input
+					onClick={() => setIsFocused(true)}
+					type="text"
+					placeholder="Search"
+				/>
 				<div className={styles.closeContainer}>
 					{isfocused && (
 						<img
 							className={styles.closeIcon}
 							src="/images/icons/icons8-close-150.png"
-							onClick={unFocus}
+							onClick={() => setIsFocused(false)}
 						/>
 					)}
 				</div>
